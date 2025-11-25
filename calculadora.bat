813b3415af734f9f2f68324cc205069c1734762b
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2025-11-25 20:02:34 +0100

    calculadora

---
+++ calculadora.bat
@@ -0,0 +1,53 @@
+@echo off
+
+:Inicio
+
+cls
+
+echo CALCULADORA
+echo S:Suma
+echo R:Resta
+echo D:Divison
+echo M:Multiplicacion
+echo E:Exit
+
+Set /p opcion=Elige una opcion (S/R/D/M/E):
+
+goto %opcion%
+
+:S
+set /p x=Introduce el primer numero:
+set /a x<0 goto %opcion%
+set /p y=Introduce el segundo numero:
+set /a z=x+y
+echo El resultado de la suma de %x% y %y% es %z%
+pause
+goto inicio
+
+:R
+set /p x=Introduce el primer numero:
+set /p y=Introduce el segundo numero:
+set /a z=x-y
+echo El resultado de la resta de %x% menos %y% es %z%
+pause
+goto Inicio
+
+:M
+set /p x=Introduce el primer numero:
+set /p y=Introduce el segundo numero:
+set /a z=x*y
+echo El resultado de la suma de %x% y %y% es %z%
+pause
+goto inicio
+
+:D
+set /p x=Introduce el primer numero:
+set /p y=Introduce el segundo numero:
+set /a z=x/y
+echo El resultado de la suma de %x% y %y% es %z%
+pause
+goto inicio
+
+:E
+pause
+exit
